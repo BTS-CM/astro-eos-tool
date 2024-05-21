ec5b3b5f112fff2b90a578c1ed5998cbd1883614
CM <grctestemail@gmail.com>
2024-05-21 16:20:36 +0100

    Update Transfering.tsx

--- src/components/react/Transfering.tsx
+++ src/components/react/Transfering.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 
@@ -12,7 +13,6 @@ import {
 } from "@/components/ui/select";
 
 import HoverInfo from "@/components/react/HoverInfo";
-import { useState } from "react";
 import DeepLinkDialog from "./DeepLinkDialog";
 
 export default function Transfer() {
@@ -23,6 +23,20 @@ export default function Transfer() {
   const [asset, setAsset] = useState<string>("EOS");
   const [memo, setMemo] = useState<string>("");
   const [openDeeplink, setOpenDeeplink] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (chain) {
+      setAsset(chain);
+    }
+  }, [chain]);
+
+  useEffect(() => {
+    if (asset === "WRAM") {
+      setQuantity("1");
+    } else if (asset === "EOS" || asset === "TLOS" || asset === "BEOS") {
+      setQuantity("0.0001");
+    }
+  }, [asset]);
 
   return (
     <div className="grid grid-cols-1">
@@ -65,36 +79,61 @@ export default function Transfer() {
           onInput={(e) => setTo(e.currentTarget.value)}
         />
         <HoverInfo
-          content={`The quantity of tokens you want to transfer to the other account.`}
-          header={`Quantity of asset to transfer`}
-        />
-        <Input
-          placeholder="1.0000"
-          value={quantity}
-          type="number"
-          min="0.0001"
-          step="0.0001"
-          onInput={(e) => {
-            let value = parseFloat(e.currentTarget.value);
-            if (!isNaN(value)) {
-              setQuantity(e.currentTarget.value);
-            }
-          }}
-          onBlur={(e) => {
-            const value = parseFloat(e.currentTarget.value);
-            setQuantity(value < 0.0001 ? "0.0001" : value.toFixed(4));
-          }}
-        />
-        <HoverInfo
           content={`The asset which you intend to transfer to the other account.`}
           header={`Asset to transfer`}
         />
-        <Input
-          placeholder="EOS"
-          value={asset}
-          type="text"
-          onInput={(e) => setAsset(e.currentTarget.value)}
+        <Select value={asset} onValueChange={setAsset}>
+          <SelectTrigger>
+            <SelectValue>{asset}</SelectValue>
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              <SelectLabel>Asset to transfer</SelectLabel>
+              {chain === "EOS" ? (
+                <>
+                  <SelectItem value="EOS">EOS</SelectItem>
+                  <SelectItem value="WRAM">WRAM</SelectItem>
+                </>
+              ) : null}
+              {chain === "TLOS" ? <SelectItem value="TLOS">TLOS</SelectItem> : null}
+              {chain === "BEOS" ? <SelectItem value="BEOS">BEOS</SelectItem> : null}
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+        <HoverInfo
+          content={`The quantity of tokens you want to transfer to the other account.`}
+          header={`Quantity of asset to transfer`}
         />
+        {asset === "EOS" || asset === "TLOS" || asset === "BEOS" ? (
+          <Input
+            placeholder="1.0000"
+            value={quantity}
+            type="number"
+            min="0.0001"
+            step="0.0001"
+            onInput={(e) => {
+              let value = parseFloat(e.currentTarget.value);
+              if (!isNaN(value)) {
+                setQuantity(e.currentTarget.value);
+              }
+            }}
+          />
+        ) : null}
+        {asset === "WRAM" ? (
+          <Input
+            placeholder="1"
+            value={quantity}
+            type="number"
+            min="1"
+            step="1"
+            onInput={(e) => {
+              let value = parseInt(e.currentTarget.value);
+              if (!isNaN(value)) {
+                setQuantity(value.toString());
+              }
+            }}
+          />
+        ) : null}
         <HoverInfo
           content="A message to associate with the transfer, readable by the recipient."
           header="Optional transfer memo"
@@ -117,7 +156,7 @@ export default function Transfer() {
           <DeepLinkDialog
             trxJSON={[
               {
-                account: "eosio.token",
+                account: asset === "WRAM" ? "eosio.wram" : "eosio.token",
                 name: "transfer",
                 authorization: [
                   {
