5a475b24411f52cc75d950e55084d689e71aac9e
CM <grctestemail@gmail.com>
2024-07-06 20:11:52 +0100

    static changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,8 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import serviceWorker from "astrojs-service-worker";
-
-import vercel from "@astrojs/vercel/serverless";
 
 export default defineConfig({
-  integrations: [serviceWorker(), tailwind(), react()],
-  output: "server",
-  adapter: vercel(),
+  integrations: [tailwind(), react()],
+  output: "static"
 });
